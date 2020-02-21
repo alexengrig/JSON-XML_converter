eed41553bcca8a12494559c1c4d1a9c36b808afc
Grig Alex <alexengrigdev@gmail.com>
2020-02-21 10:35:43 +0300

    Add "Uncomment a line" problem solution

---
+++ Problems/Uncomment a line/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * The class implements a simple application with the main method.
+ */
+class CommentedSimpleApplication {
+
+    public static void main(String[] args) {
+	  // System.out.println(980233);
+	  System.out.println(893457);
+	  // System.out.println(789722);
+    }
+}
