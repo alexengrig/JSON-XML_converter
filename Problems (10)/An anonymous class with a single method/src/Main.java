b1f1c4bd6932d8237ad4572cabd9b67fb07c551f
Grig Alex <alexengrigdev@gmail.com>
2020-03-01 14:47:21 +0300

    Add "An anonymous class with a single method" problem solution

---
+++ Problems (10)/An anonymous class with a single method/src/Main.java
@@ -0,0 +1,6 @@
+SingleMethodInterface instance=new SingleMethodInterface(){
+@Override
+public void doSomething(){
+        System.out.println("The anonymous class does something");
+        }
+        };
