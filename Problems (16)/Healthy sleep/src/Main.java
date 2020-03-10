6bf2fee2b29ee0ca86a1bca5ac6e30b6c255e549
Grig Alex <alexengrigdev@gmail.com>
2020-03-10 19:53:09 +0300

    Add "Healthy sleep" problem solution

---
+++ Problems (16)/Healthy sleep/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int a = scanner.nextInt();
+        final int b = scanner.nextInt();
+        final int h = scanner.nextInt();
+        if (h >= a && h <= b) {
+            System.out.println("Normal");
+        } else if (h < a) {
+            System.out.println("Deficiency");
+        } else {
+            System.out.println("Excess");
+        }
+    }
+}
