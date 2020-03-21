6ac45045218125d221b18a2bcc40712b5c428fbf
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 23:09:53 +0300

    Add "From Double to Integer" problem solution

---
+++ Problems (21)/From Double to Integer/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+    public static int convert(Double val) {
+        if (val.isNaN()) {
+            return 0;
+        } else if (Double.POSITIVE_INFINITY == val) {
+            return Integer.MAX_VALUE;
+        } else if (Double.NEGATIVE_INFINITY == val) {
+            return Integer.MIN_VALUE;
+        } else {
+            return val.intValue();
+        }
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double doubleVal = new Double(scanner.nextDouble() / scanner.nextDouble());
+        System.out.println(convert(doubleVal));
+    }
+}
