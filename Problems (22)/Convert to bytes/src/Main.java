0e1ccd1bb4e306d70a229e089851ee78def93a16
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 23:33:20 +0300

    Add "Convert to bytes" problem solution

---
+++ Problems (22)/Convert to bytes/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        try (InputStream inputStream = System.in) {
+            int read;
+            while ((read = inputStream.read()) != -1) {
+                System.out.print(read);
+            }
+        }
+    }
+}
