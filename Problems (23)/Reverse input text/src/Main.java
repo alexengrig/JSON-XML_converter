01f14cfe9135f438949c0d5528fef6bae496a8e6
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 23:47:10 +0300

    Add "Reverse input text" problem solution

---
+++ Problems (23)/Reverse input text/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        try (Reader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            final StringBuilder builder = new StringBuilder();
+            final char[] buffer = new char[1024];
+            int count;
+            while ((count = reader.read(buffer)) != -1) {
+                builder.append(buffer, 0, count);
+            }
+            System.out.println(builder.reverse());
+        }
+    }
+}
