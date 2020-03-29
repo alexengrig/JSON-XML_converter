3c59659375ef8d4f9bd531678df9e25d4adcda93
Grig Alex <alexengrigdev@gmail.com>
2020-03-29 23:48:34 +0300

    Add "Frequency of characters" problem solution

---
+++ Problems (26)/Frequency of characters/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> letters = Arrays.asList(scanner.nextLine().split("\\s+"));
+        final int frequency = Collections.frequency(letters, scanner.next());
+        System.out.println(frequency);
+    }
+}
