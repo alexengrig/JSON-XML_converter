c9390bba4b965c6457df884d162f0cb1a24135dd
Grig Alex <alexengrigdev@gmail.com>
2020-04-01 21:20:30 +0300

    Add "Is between" problem solution

---
+++ Problems (30)/Is between/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(c >= b ? a >= b && c >= a : a >= c && b >= a);
+    }
+}
