c8b740859cc19e2bdefb2d47f01317d296ac76d2
Grig Alex <alexengrigdev@gmail.com>
2020-04-10 23:01:35 +0300

    Add "Boxes" problem solution

---
+++ Problems (38)/Boxes/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int x1 = scanner.nextInt();
+        final int y1 = scanner.nextInt();
+        final int z1 = scanner.nextInt();
+        final int x2 = scanner.nextInt();
+        final int y2 = scanner.nextInt();
+        final int z2 = scanner.nextInt();
+        final int[] sizes1 = {x1, y1, z1};
+        final int[] sizes2 = {x2, y2, z2};
+        Arrays.sort(sizes1);
+        Arrays.sort(sizes2);
+        if (Arrays.equals(sizes1, sizes2)) {
+            System.out.println("Box 1 = Box 2");
+        } else if (sizes1[2] <= sizes2[2] && sizes1[0] <= sizes2[0]) {
+            System.out.println("Box 1 < Box 2");
+        } else if (sizes1[2] >= sizes2[2] && sizes1[0] >= sizes2[0]) {
+            System.out.println("Box 1 > Box 2");
+        } else {
+            System.out.println("Incomparable");
+        }
+    }
+}
