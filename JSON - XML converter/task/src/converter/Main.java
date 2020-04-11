73266639e9d75f21fd2653948c652a50181308c6
Grig Alex <alexengrigdev@gmail.com>
2020-04-11 15:26:13 +0300

    Refactor Main

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -3,14 +3,8 @@ package converter;
 import converter.js.JsConverter;
 import converter.js.JsObject;
 import converter.js.JsParser;
-import converter.js.JsPrinter;
-import converter.json.JsonConverter;
-import converter.json.JsonElement;
-import converter.json.JsonParser;
 import converter.x.XElement;
 import converter.x.XPrinter;
-import converter.xml.XmlElement;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileReader;
@@ -27,41 +21,6 @@ public class Main {
         }
     }
 
-    @NotNull
-    private static String getExample1() {
-        return "{\n" +
-                "  \"transaction\": {\n" +
-                "    \"inner13\": {\n" +
-                "      \"@invalid_attr\": {\n" +
-                "        \"some_key\": \"some value\"\n" +
-                "      },\n" +
-                "      \"#inner13\": {\n" +
-                "        \"key\": \"value\"\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"\": {\n" +
-                "      \"#\": null,\n" +
-                "      \"secret\": \"this won't be converted\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
-    }
-
-    private static void task() throws IOException {
-        final String input = getInputFromFile();
-        if (isXML(input)) {
-
-        } else if (isJSON(input)) {
-            final JsonParser jsonParser = new JsonParser();
-            final JsonElement jsonElement = jsonParser.parse(input);
-            final JsonConverter jsonConverter = new JsonConverter();
-            final XmlElement xmlElement = jsonConverter.convert(jsonElement);
-            System.out.println(xmlElement);
-        } else {
-            throw new IllegalArgumentException("Unknown type");
-        }
-    }
-
     private static String getInputFromFile() throws IOException {
         final File file = new File("test.txt");
         try (final FileReader reader = new FileReader(file)) {
@@ -73,13 +32,5 @@ public class Main {
             }
             return builder.toString();
         }
-    }
-
-    private static boolean isJSON(String input) {
-        return input.startsWith("{");
-    }
-
-    private static boolean isXML(String input) {
-        return input.startsWith("<");
     }
 }
