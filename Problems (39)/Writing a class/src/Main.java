f4e973f131ad6af0abf8b001dba3174658e05165
Grig Alex <alexengrigdev@gmail.com>
2020-04-12 14:56:44 +0300

    Add "Writing a class" problem solution

---
+++ Problems (39)/Writing a class/src/Main.java
@@ -0,0 +1,11 @@
+class Shelf<T extends Book>{
+    private T element;
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+}
