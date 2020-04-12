bf6b0837482b1fe144e3688457ad505e80aff143
Grig Alex <alexengrigdev@gmail.com>
2020-04-12 16:26:48 +0300

    Add "List multiplicator" problem solution

---
+++ Problems (40)/List multiplicator/src/Main.java
@@ -0,0 +1,29 @@
+/**
+ * Class to modify
+ */
+class ListMultiplicator {
+
+    /**
+     * Multiplies original list provided numper of times
+     *
+     * @param list list to multiply
+     * @param n    times to multiply, should be zero or greater
+     */
+    public static void multiply(List<?> list, int n) {
+        if (n == 0) {
+            list.clear();
+        } else {
+			anotherMultiply(list, n);
+        }
+    }
+
+	public static <T> void anotherMultiply(List<T> list, int n) {
+		final int size = list.size();
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < size; j++) {
+				list.add(list.get(j));
+			}
+		}
+	}
+
+}
