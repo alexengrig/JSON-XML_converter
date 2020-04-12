3b2ea9a3b1ffebbcd19fe2832212828de3ea5f74
Grig Alex <alexengrigdev@gmail.com>
2020-04-12 16:41:03 +0300

    Add "Packing backeries" problem solution

---
+++ Problems (41)/Packing backeries/src/Main.java
@@ -0,0 +1,11 @@
+/**
+    This packer has too much freedom and could repackage stuff in wrong direction.
+    Fix method types in signature and add implementation. 
+*/
+class Packer {
+
+	public void repackage(Box<? super Bakery> to, Box<? extends Bakery> from) {
+		to.put(from.get());
+	}
+
+}
