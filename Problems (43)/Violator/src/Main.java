37943a07aa427fd01416fb7d9389ea821dc4d835
Grig Alex <alexengrigdev@gmail.com>
2020-04-13 21:44:43 +0300

    Add "Violator" problem solution

---
+++ Problems (43)/Violator/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Class to work with
+ */
+class Violator {
+
+	public static List<Box<? extends Bakery>> defraud() {
+		final ArrayList list = new ArrayList();
+		final Box box = new Box<>();
+		box.put(new Paper());
+		list.add(box);
+		return list;
+	}
+
+}
