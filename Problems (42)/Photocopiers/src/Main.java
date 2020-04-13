d75a305ad06ecb9744fd0d67397181ee28660622
Grig Alex <alexengrigdev@gmail.com>
2020-04-13 21:32:58 +0300

    Add "Photocopiers" problem solution

---
+++ Problems (42)/Photocopiers/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Class to work with
+ */
+class Multiplicator {
+
+	public static <T extends Copy<T>> Folder<T>[] multiply(Folder<T> folder, int arraySize) {
+		final Folder<T>[] folders = new Folder[arraySize];
+		for (int i = 0; i < arraySize; i++) {
+			final Folder target = new Folder<>();
+			target.put(folder.get().copy());
+			folders[i] = target;
+		}
+		return folders;
+	}
+
+}
