959ad4c782c407087878a615b9872d0894fc8563
Grig Alex <alexengrigdev@gmail.com>
2020-04-14 22:56:34 +0300

    Add "Sum array elements greater than a value" problem solution

---
+++ Problems (44)/Sum array elements greater than a value/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int count = scanner.nextInt();
+        final int[] arr = new int[count];
+        for (int i = 0; i < count; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        final int n = scanner.nextInt();
+        int sum = 0;
+        for (int i : arr) {
+            if (i > n) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
