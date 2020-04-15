043d7177bdc169276ea4a9e952a7b643422874bb
Grig Alex <alexengrigdev@gmail.com>
2020-04-15 22:13:50 +0300

    Add "Fizz Buzz" problem solution

---
+++ Problems (45)/Fizz Buzz/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int a = scanner.nextInt();
+        final int b = scanner.nextInt();
+        for (int i = a; i <= b; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
