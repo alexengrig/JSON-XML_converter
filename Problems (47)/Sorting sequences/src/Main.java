285677cdd94e72f2d6562a7f46f79ec81a215e95
Grig Alex <alexengrigdev@gmail.com>
2020-04-17 18:35:37 +0300

    Add "Sorting sequences" problem solution

---
+++ Problems (47)/Sorting sequences/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void sortInDescendingOrder(List<Integer> sequence) {
+        sequence.sort(Comparator.reverseOrder());
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final List<Integer> seq = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        sortInDescendingOrder(seq);
+        seq.forEach(e -> System.out.print(e + " "));
+    }
+}
