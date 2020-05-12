811822d6b35f9f659e2b456d4528e3ee3fdf6b50
Grig Alex <alexengrigdev@gmail.com>
2020-05-12 20:36:59 +0300

    Add "Output elements" problem solution

---
+++ Problems/Output elements/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<String> nameSet = new TreeSet<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+        nameSet.forEach(System.out::println);
+    }
+}
