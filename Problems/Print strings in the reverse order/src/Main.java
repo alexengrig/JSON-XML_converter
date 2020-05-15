7f7bcfa336c08a9cfad2be48da221faf2311d288
Grig Alex <alexengrigdev@gmail.com>
2020-05-15 11:21:28 +0300

    Add "Print strings in the reverse order" problem solution

---
+++ Problems/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,15 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final String first = scanner.nextLine();
+        final String second = scanner.nextLine();
+        final String third = scanner.nextLine();
+        System.out.println(third);
+        System.out.println(second);
+        System.out.println(first);
+    }
+}
