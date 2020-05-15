f9a31a94b8c5a1be5a4f7a03cc21430d80429d1f
Grig Alex <alexengrigdev@gmail.com>
2020-05-15 21:27:35 +0300

    Add toPretty with level to JsonArray

--- JSON - XML converter/task/src/converter/json/JsonArray.java
+++ JSON - XML converter/task/src/converter/json/JsonArray.java
@@ -20,6 +20,18 @@ public class JsonArray extends JsonValue {
     }
 
     @Override
+    public String toPretty(int level) {
+        final int nextLevel = level + 1;
+        final String nextIndent = INDENT.repeat(nextLevel);
+        final StringJoiner joiner = new StringJoiner(",\n" + nextIndent);
+        for (JsonValue value : values) {
+            joiner.add(value.toPretty(nextLevel));
+        }
+        final String indent = INDENT.repeat(level);
+        return String.format("[\n" + nextIndent + "%s\n" + indent + "]", joiner);
+    }
+
+    @Override
     public String toString() {
         final StringJoiner joiner = new StringJoiner(", ");
         for (JsonValue value : values) {
